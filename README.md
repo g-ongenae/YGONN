f3dc029ccaa4d61540decb2f6447ba1b81f83bd9
Guillaume Ongenae <guillaume.ongenae@gmail.com>
2018-03-25 14:47:38 +0200

    Fixes
    Lint:
    - prefer-const & no-var
    - missing semi-colon
    - prefer console.error on err
    Add little docs
    Fix:
    - did you run train()? check function was executing before trainning one
    - value1.concat is not a function

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # YGONN
+
 Yu Gi Oh cards recognization in js using brain.js (Neural Network)
+
+## Install
+
+```bash
+npm i
+```
+
+## Run
+
+```bash
+npm start
+```
+
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const brain = require("brain.js")
     , request = require("request")
-    , getPixels = require("get-pixels")
+    , getPixels = require("get-pixels");
 
 const hostListCard = "https://www.ygohub.com/api/all_cards";
 
-let net = new brain.recurrent.RNN();
+const net = new brain.recurrent.RNN();
 
-let getYGOHub = async function(url, map) {
+const getYGOHub = async function(url, map) {
   return await new Promise((resolve, reject) => {
     request(url, function (error, response, body) {
       if(error) {
@@ -21,17 +21,17 @@ let getYGOHub = async function(url, map) {
   })
 }
 
-let listAllCard = async function() {
+const listAllCard = async function() {
   return await getYGOHub('https://www.ygohub.com/api/all_cards', JSON.parse);
 }
 
-let getInfoFromCardName = async function(name) {
+const getInfoFromCardName = async function(name) {
   console.log(name);
-  let url = `https://www.ygohub.com/api/card_info?name=${name}`
+  const url = `https://www.ygohub.com/api/card_info?name=${name}`;
   return await getYGOHub(url, JSON.parse);
 }
 
-let getImageCard = async function(card) {
+const getImageCard = async function(card) {
   return await new Promise((resolve, reject) => {
     getPixels(card.image_path, (err, pixels) => {
       if(err) {
@@ -43,51 +43,53 @@ let getImageCard = async function(card) {
   });
 }
 
-let getRandomInt = function(min, max) {
+const getRandomInt = function(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-let training = async function(nbTrain, cards) {
-  let testSet = [];
-  for (var i = 0; i < nbTrain; i++) {
-    let name = cards[getRandomInt(0, cards.length)];
+const training = async function(nbTrain, cards) {
+  const testSet = [];
+  for (let i = 0; i < nbTrain; i++) {
+    const name = cards[getRandomInt(0, cards.length)];
     try {
-      let card = (await getInfoFromCardName(name)).card;
-      let image = (await getImageCard(card)).data;
-      testSet.push({input: image, output: card.type});
+      const card = (await getInfoFromCardName(name)).card;
+      const image = (await getImageCard(card)).data;
+      testSet.push({input: [image], output: card.type});
     } catch(err) {
-      console.log(err, name);
+      console.error(err);
     }
   }
-  net.train(testSet);
+  
+  const res = net.train(testSet);
+  console.log("Training result:", res);
 }
 
-let check = async function(n, cards) {
+const check = async function(n, cards) {
   let sat = 0;
 
-  for (var i = 0; i < n; i++) {
-    let name = cards[i];
+  for (let i = 0; i < n; i++) {
+    const name = cards[i];
     try {
-      let card = (await getInfoFromCardName(name)).card;
-      let image = (await getImageCard(card)).data;
-      let output = net.run(image);
+      const card = (await getInfoFromCardName(name)).card;
+      const image = (await getImageCard(card)).data;
+      const output = net.run([image]);
       console.log(output);
       if(output === card.type) {
         sat++;
       }
     } catch(err) {
-      console.log(err, name);
+      console.error(err);
     }
   }
-  console.log("Result:");
-  console.log("Good: ", sat / n);
-  console.log("Bad: ", (1 - sat0) / n);
+  console.log(`Result:
+  Good: ${sat / n}
+  Bad:  ${(1 - sat) / n}`);
 }
 
-let main = async function() {
-  let allCards = (await listAllCard()).cards;
-  training(200, allCards);
-  check(10, allCards);
+const main = async function() {
+  const allCards = (await listAllCard()).cards;
+  await training(200, allCards);
+  await check(10, allCards);
 }
 
 main();
@@ -97,7 +99,7 @@ main();
 //   {input: [0,1], output: [1]},
 //   {input: [1,0], output: [1]},
 //   {input: [1,1], output: [0]}
-// ])
+// ]);
 //
-// let output = net.run([0,0]);
+// const output = net.run([0,0]);
 // console.log(output);
